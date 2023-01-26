9b6589cbd64a5855444164be91cc13d3adc65344
Brett Feltmate <brettfeltmate@MacBook-Air-3.local>
2023-01-26 19:46:33 -0400

    Honestly, don't remember. Mostly minor adds I'm pretty sure.

--- experiment.py
+++ experiment.py
@@ -5,9 +5,12 @@ __author__ = "Brett Feltmate"
 import klibs
 from klibs import P
 from klibs.KLConstants import STROKE_CENTER
-from klibs.KLUtilities import deg_to_px
+from klibs.KLUtilities import deg_to_px 
+from klibs.KLGraphics import fill, blit, flip
 from klibs.KLGraphics import KLDraw as kld
 from klibs.KLBoundary import BoundarySet, CircleBoundary
+from klibs.KLCommunication import message
+from klibs.KLResponseCollectors import KeyPressResponse
 
 WHITE = (255, 255, 255, 255)
 BLACK = (0, 0, 0, 255)
@@ -15,12 +18,11 @@ BLACK = (0, 0, 0, 255)
 class RetinoSpatioIOR(klibs.Experiment):
 
 	def setup(self):
-		offset = deg_to_px(6)
 
 		"""
 		Layout
 		Offset - Stimuli offset in increments of 6º along cardinal directions, respective to screen centre
-		[] - Placeholders, 1.5º
+		[] - Placeholders, 1.5º; denoted 0-7 from top-left to bottom-right
 		() - Saccade locations, 1.5º
 		+  - Fixation, 1.5º
 		*  - Target, 0.75º
@@ -39,23 +41,26 @@ class RetinoSpatioIOR(klibs.Experiment):
 
 		[]             []
 		"""
+		# Stimuli are offset from each other by units of 6º
+		offset = deg_to_px(6)
 
-
+		# Point positions to serve as anchors when positioning stimuli
 		self.locations = {
 			'placeholders': {
-				0: (P.screen_c[0] - offset, P.screen_c[1] - (offset*3)),
-				1: (P.screen_c[0] + offset, P.screen_c[1] - (offset*3)),
-				2: (P.screen_c[0] - offset, P.screen_c[1] - offset),
-				3: (P.screen_c[0] + offset, P.screen_c[1] - offset),
-				4: (P.screen_c[0] - offset, P.screen_c[1] + offset),
-				5: (P.screen_c[0] + offset, P.screen_c[1] + offset),
-				6: (P.screen_c[0] - offset, P.screen_c[1] + (offset * 3)),
-				7: (P.screen_c[0] + offset, P.screen_c[1] + (offset * 3)),
+				1: (P.screen_c[0] - offset, P.screen_c[1] - (offset*1.5)),
+				2: (P.screen_c[0] + offset, P.screen_c[1] - (offset*1.5)),
+				3: (P.screen_c[0] - offset, P.screen_c[1] - offset),
+				4: (P.screen_c[0] + offset, P.screen_c[1] - offset),
+				5: (P.screen_c[0] - offset, P.screen_c[1] + offset),
+				6: (P.screen_c[0] + offset, P.screen_c[1] + offset),
+				7: (P.screen_c[0] - offset, P.screen_c[1] + (offset*1.5)),
+				8: (P.screen_c[0] + offset, P.screen_c[1] + (offset*1.5)),
 			},
+			# Gaze & saccade locations
 			'fixation': {
-				'upper': (P.screen_c[0], P.screen_c[1] - (offset*2)),
-				'middle': P.screen_c,
-				'lower': (P.screen_c[0], P.screen_c[1] + (offset*2)),
+				'upper': (P.screen_c[0], P.screen_c[1] - offset),
+				'center': P.screen_c,
+				'lower': (P.screen_c[0], P.screen_c[1] + offset),
 			}
 		}
 
@@ -69,9 +74,11 @@ class RetinoSpatioIOR(klibs.Experiment):
 		Cue - Thickening of placeholder
 		"""
 
+		# uncued = default stroke, cued = only applied to the cued location
 		uncued_stroke = [deg_to_px(0.1), WHITE, STROKE_CENTER]
 		cued_stroke = [deg_to_px(0.3), WHITE, STROKE_CENTER]
 
+		# Visual assets
 		self.stimuli = {
 			"placeholder":
 				kld.Rectangle(
@@ -102,15 +109,8 @@ class RetinoSpatioIOR(klibs.Experiment):
 				)
 		}
 
-		"""
-		Eye-tracker boundaries (for checking fixation & saccade locations)
 		
-		Boundaries:
-			Fixation-middle
-			Fixation-upper
-			Fixation-lower
-		"""
-
+		# Eye-tracker boundaries (for montoring fixations & saccades)
 		self.gaze = BoundarySet([
 			CircleBoundary(
 				label = "upper", 
@@ -118,8 +118,8 @@ class RetinoSpatioIOR(klibs.Experiment):
 				radius = deg_to_px(1.5)
 			),
 			CircleBoundary(
-				label = "middle",
-				center = self.locations['fixation']['middle'],
+				label = "center",
+				center = self.locations['fixation']['center'],
 				radius = deg_to_px(1.5)
 			),
 			CircleBoundary(
@@ -130,7 +130,17 @@ class RetinoSpatioIOR(klibs.Experiment):
 		])
 
 
+		self.error_msgs = {
+			"EarlySaccade":  message("Moved eyes too soon!", blit_txt=False),
+			"MissedSaccade": message("No eye movement detected!", blit_txt=False),
+			"WrongSaccade":  message("Moved eyes in wrong direction!", blit_txt=False),
+			"EarlyResponse": message("Please wait until the target appears to respond!", blit_txt=False)
+		}
 
+		self.rc.uses[KeyPressResponse]
+		self.rc.keypress_listener.key_map = {"spacebar": "spacebar"}
+
+ 
 		"""
 
 		Sequence:
@@ -148,24 +158,33 @@ class RetinoSpatioIOR(klibs.Experiment):
 
 
 		"""
-		pass
 
 	def block(self):
 		pass
 
 	def setup_response_collector(self):
+		# I don't think anything needs to go here
 		pass
 
 	def trial_prep(self):
+		# Set location of saccade cue (dependent on pro/anti group)
+		# Set location of target (dependent on saccade location)
+
+		
 		pass
 
 	def trial(self):
 		"""
 		:return:
+		practicing: True or False
+		saccade_condition: pro or anti
+		saccade_cue_loc: up or down
+		target_cue_loc: 3-6
+		target_loc: 1-8
+		button_rt
+		saccade_rt
 
-		saccade_condition: pro OR anti
-		saccade_cue_loc: up OR down
-		target_cue_loc:
+
 
 		"""
 
